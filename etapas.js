6f744eaa3f768987e5a8d13b0fcd9a39f1011ed0
kelvyntelles <kelvyn.telles@gmail.com>
2021-12-28 11:18:00 -0300

    controle das areas da tela js

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url:'cachorro.jpg', legenda: 'vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url:'gato.jpg', legenda: 'vereador'}
+                ]
+            }
+        ],
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url:'prefeito.jpg', legenda: 'Prefeiro'},
+                    {url:'vice.jpg', legenda: 'Vice-Prefeito', smart: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url:'cachrro.jpg', legenda: 'Prefeiro'},
+                    {url:'gato.jpg', legenda: 'Vice-Prefeito', smart: true}
+                ]
+            }
+        ],
+    }
+];
